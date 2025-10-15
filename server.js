6603ca6267e6bb61b7261164144351d8934f2bb4
heerthakor <145858330+heerthakor@users.noreply.github.com>
2025-10-15 13:01:08 +0530

    Update server.js

--- server.js
+++ server.js
@@ -262,6 +262,5 @@ app.use((req, res) => {
 
 // ----------------- Start Server -----------------
 const PORT = process.env.PORT || 3003;
-app.listen(PORT, "0.0.0.0", () => {
-  console.log(`✅ Server running on port ${PORT}`);
-});
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+
